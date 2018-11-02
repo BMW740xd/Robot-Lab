3989f3c46b199ca491d36895bda166be34bad541
mariavu <maria-vy@hotmail.com>
2018-11-02 22:11:46 +0100

    endring i sensob

--- Sensob.py
+++ Sensob.py
@@ -1,14 +1,57 @@
+from ultrasonic import *
+from camera import *
+from reflectance_sensors import *
+
 class Sensob:
 
-    #Sender inn et nummer under initialiseringen for å si hva slags type SensorObjekt dette skal være
-    #Vel den letteste måten for å få opprettet ulike objekter
+#Oppretter en liste med sensorer som er tilknyttet til denne sensoben
+    def __init__(self,sensor):
+        self.sensors=[sensor]
+        self.value=None
 
-    def __init__(self, nummer):
-        self.type_nummer = nummer
+
+    def update(self,sensor):
         pass
 
-    def update(self):
-        pass
+    def get_value(self):
+        return self.value
 
-    def reset_sensor(self):
-        pass
+    def reset(self):
+        self.value=None
+
+class UltrasonicSensob(Sensob):
+
+    def __init__(self,sensor):
+        super().__init__(sensor)
+
+    def update(self,sensor):
+        #Sjekker om sensoren i listen med sensorer er av type "ultrasonic"
+        for sensor in self.sensors:
+            if isinstance(sensor,Ultrasonic):
+                #setter verdien til sensob til verdien(float) som ultrasonic gir, det er altså avstanden til en hindring
+                self.value=sensor.get_value()
+
+class ReflectanceSensob(Sensob):
+
+    def __init__(self,sensor):
+        super().__init__(sensor)
+
+    def update(self,sensor):
+        #Sjekker om sensoren i listen med sensorer er av type "reflectance"
+        for sensor in self.sensors:
+            if isinstance(sensor,ReflectanceSensors):
+                #setter verdien til sensob til et array av 6 elementer, tallene varierer mellom 0 og 1
+                #høye tall indikerer lyse farger, lave tall indikerer mørke farger
+                #sier noe om hvor roboten befinner seg i forhold til en linje
+                self.value=sensor.get_value()
+
+class CameraSensob(Sensob):
+
+    def __init__(self,sensor):
+        super().__init__(sensor)
+
+    def update(self,sensor):
+        #Sjekker om sensoren i listen med sensorer er av type "camera"
+        for sensor in self.sensors:
+            if isinstance(sensor,Camera):
+                self.value=sensor.get_value()
