86ea76752a7487964121386cc11b2e79fc8339dc
mariavu <maria-vy@hotmail.com>
2018-11-03 13:55:45 +0100

    endring i motob

--- Motob.py
+++ Motob.py
@@ -6,14 +6,14 @@ class Motob:
         self.motors=Motors() #Oppretter en liste med motorer
         self.value=None #et sted hvor man lagrer motor recommendations sendt til denne motoben
         #lager en dictionary med de forskjellige bevegelsene til motorene
-        self.settings={"forward":self.motors.forward(),
-                       "backward":self.motors.backward(),
-                       "left":self.motors.left(),
-                       "right":self.motors.right(),
-                       "stop":self.motors.stop()}
+        self.settings={"F":self.motors.forward(),
+                       "B":self.motors.backward(),
+                       "L":self.motors.left(),
+                       "R":self.motors.right(),
+                       "S":self.motors.stop()}
 
     def operationalize(self,recommendation):
-        #sjekker om recommendation er i dictionay
+        #sjekker om recommendation er i dictionary
         if recommendation in self.settings:
             #utfører recommendation
             self.settings.get(recommendation)
@@ -27,3 +27,6 @@ class Motob:
         else:
             self.value=recommendation #setter value til recommendation
             self.operationalize(recommendation) #utfører recommendation
+
+    def get_value(self):
+        return self.value #returnerer motor recommendation
