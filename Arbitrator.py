6f9df5472de0f3eaa80b3c23ddf373d5bb8aa9cf
mariavu <maria-vy@hotmail.com>
2018-11-06 11:46:14 +0100

    endring i ab

--- Arbitrator.py
+++ Arbitrator.py
@@ -11,13 +11,14 @@ class Arbitrator:
     def choose_action(self):
         #velger den oppførselen med størst vekt
         winner=self.active_behaviors[0].weight
+        winning_action=self.active_behaviors[0]
         action=() #lager en tuppel
-        for i in range(1,len(self.active_behaviors)+1):
-            winning_action=None
-            if self.active_behaviors[i].weight>winner:
-                winning_action=self.active_behaviors[i]
-            action=(winning_action,winning_action.consider_deactivation())
-        return action
+        for i in range(1,len(self.active_behaviors)+1): #går gjennom alle elementene i listen med active_behaviors
+            if self.active_behaviors[i].weight>winner: #sjekker om neste behavior er større en winner
+                winner=self.active_behaviors[i].weight #setter winner til den nye behavior med større vekt
+                winning_action=self.active_behaviors[i] #setter winning_action til den nye behavior
+            action=(winning_action,winning_action.halt_request) #legger til behavior og en boolean om behavior skal bli stoppet
+        return action #returnerer tuppelen
 
 
 
