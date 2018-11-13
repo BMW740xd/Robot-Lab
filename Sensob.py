f0ccbf181369d6ffe88f93c2837f0aa40242b20f
mariavu <maria-vy@hotmail.com>
2018-11-13 13:39:17 +0100

    endring i sen

--- Sensob.py
+++ Sensob.py
@@ -7,54 +7,28 @@ class Sensob:
 
     # Oppretter en liste med sensorer som er tilknyttet til denne sensoben
     def __init__(self, sensor):
-        self.sensors = [sensor]
+        self.sensor = sensor # tall som sier hvilken sensor dette er
         self.value = None
 
     def update(self):
-        pass
+            if self.sensor == 1: #sjekker om sensoren er av type "ultrasonic"
+                    # setter verdien til sensob til verdien(float) som ultrasonic gir, det er altså avstanden til en hindring
+                    self.value = Ultrasonic().get_value()
+
+            elif self.sensor==2: #sjekker om sensoren er av type "camera"
+                # setter verdien til sensob til et array av 6 elementer, tallene varierer mellom 0 og 1
+                # høye tall indikerer lyse farger, lave tall indikerer mørke farger
+                # sier noe om hvor roboten befinner seg i forhold til en linje
+                self.value = Camera().get_value()
+
+            elif self.sensor==3: #sjekker om sensoren er av type "IR"
+                # setter verdien til sensob til et array av 6 elementer, tallene varierer mellom 0 og 1
+                # høye tall indikerer lyse farger, lave tall indikerer mørke farger
+                # sier noe om hvor roboten befinner seg i forhold til en linje
+                self.value = ReflectanceSensors().get_value()
 
     def get_value(self):
         return self.value
 
     def reset(self):
         self.value = None
-
-
-class UltrasonicSensob(Sensob):
-
-    def __init__(self, sensor):
-        super().__init__(sensor)
-
-    def update(self):
-        # Sjekker om sensoren i listen med sensorer er av type "ultrasonic"
-        for sensor in self.sensors:
-            if isinstance(sensor, Ultrasonic):
-                # setter verdien til sensob til verdien(float) som ultrasonic gir, det er altså avstanden til en hindring
-                self.value = sensor.get_value()
-
-
-class ReflectanceSensob(Sensob):
-
-    def __init__(self, sensor):
-        super().__init__(sensor)
-
-    def update(self):
-        # Sjekker om sensoren i listen med sensorer er av type "reflectance"
-        for sensor in self.sensors:
-            if isinstance(sensor, ReflectanceSensors):
-                # setter verdien til sensob til et array av 6 elementer, tallene varierer mellom 0 og 1
-                # høye tall indikerer lyse farger, lave tall indikerer mørke farger
-                # sier noe om hvor roboten befinner seg i forhold til en linje
-                self.value = sensor.get_value()
-
-
-class CameraSensob(Sensob):
-
-    def __init__(self, sensor):
-        super().__init__(sensor)
-
-    def update(self):
-        # Sjekker om sensoren i listen med sensorer er av type "camera"
-        for sensor in self.sensors:
-            if isinstance(sensor, Camera):
-                self.value = sensor.get_value()
